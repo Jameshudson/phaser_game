9b4de8339d0ee1fcc5d7adc73da5e9d331f95f23
James Hudson <james@nimbushosting.co.uk>
2018-07-31 21:56:46 +0100

    Setting up my game

--- src/states/Boot.js
+++ src/states/Boot.js
@@ -5,7 +5,7 @@ import config from '../config';
 export default class extends Phaser.State {
   init() {
     this.stage.backgroundColor = '#EDEEC9'
-    this.fontsReady = false
+    this.fontsReady = true
     this.fontsLoaded = this.fontsLoaded.bind(this)
   }
 
--- src/states/Game.js
+++ src/states/Game.js
@@ -1,36 +1,29 @@
 /* globals __DEV__ */
-import Phaser from 'phaser'
-import Mushroom from '../sprites/Mushroom'
-import lang from '../lang'
+import Ship from '../sprites/Ship'
 
 export default class extends Phaser.State {
-  init() { }
-  preload() { }
+  init() {
+    this.stage.backgroundColor = '#EDEEC9'
+  }
+  preload() {
+    this.load.image('Ship', 'assets/images/ship.png')
+  }
 
   create() {
-    const bannerText = lang.text('welcome')
-    let banner = this.add.text(this.world.centerX, this.game.height - 80, bannerText, {
-      font: '40px Bangers',
-      fill: '#77BFA3',
-      smoothed: false
-    })
 
-    banner.padding.set(10, 16)
-    banner.anchor.setTo(0.5)
-
-    this.mushroom = new Mushroom({
+    this.Ship = new Ship({
       game: this.game,
       x: this.world.centerX,
       y: this.world.centerY,
-      asset: 'mushroom'
+      asset: 'Ship'
     })
 
-    this.game.add.existing(this.mushroom)
+    this.game.add.existing(this.Ship)
   }
 
   render() {
     if (__DEV__) {
-      this.game.debug.spriteInfo(this.mushroom, 32, 32)
+      // this.game.debug.spriteInfo(this.Ship, 32, 32)
     }
   }
 }
